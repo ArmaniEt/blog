4dad465302d235024d249a2ad5e8f076b8d2776e
armani <razzario@gmail.com>
2018-12-15 21:45:17 +0600

    Created models user and article and migrated them

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Имя пользователя')
+    email = models.CharField(max_length=100, verbose_name='Почта')
+    favorites = models.ManyToManyField('Article', blank=True, related_name='marked_by', verbose_name='Избранное')
+
+    def __str__(self):
+        return '%s %s' % (self.name, self.email)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    author = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='Автор')
+
+    def __str__(self):
+        return self.title
