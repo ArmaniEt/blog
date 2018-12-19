3b3b28a8986d873b1f9c7a1ec6871dc95ec2d5b8
armani <razzario@gmail.com>
2018-12-19 16:41:28 +0600

    Added search form

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,10 +1,19 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, FormView
 from webapp.models import Article, User
+from webapp.forms import ArticleSearchForm
 
 
-class ArticleListView(ListView):
+class ArticleListView(ListView, FormView):
     template_name = 'index.html'
     model = Article
+    form_class = ArticleSearchForm
+
+    def get_queryset(self):
+        article_name = self.request.GET.get('article_name')
+        if article_name:
+            return self.model.objects.filter(title__icontains=article_name)
+        else:
+            return self.model.objects.all()
 
 
 class ArticleDetailView(DetailView):
